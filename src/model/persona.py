cef8d4ba155ee5759de77f012b77b5c6ed81b14a
Anderson Luis Cáceres Padilla <al.caceres@uniandes.edu.co>
2021-03-14 16:31:59 -0500

    A simple modification

--- src/model/persona.py
+++ src/model/persona.py
@@ -14,7 +14,7 @@ class Persona:
        self.__nombre = nombre
 
    def dar_edad(self):
-       return(self.__edad)
+       return(1)#(self.__edad)
 
    def dar_nombre(self):
        return(self.__nombre)
